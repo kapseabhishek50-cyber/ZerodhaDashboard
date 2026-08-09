e8c4d2ca79ff8be4c749f4ffe9069c2361461d18
Abhishek Kapse <kapseabhishek50@gmail.com>
2026-08-09 13:33:03 +0530

    Dashboard Code of Zerodha

---
+++ src/components/Apps.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "../index.css";
+import Home from "../components/Home";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const Apps = () => {
+  return (
+    <div>
+      Apps
+    </div>
+  )
+};
+
+export default Apps;
---
+++ src/components/Orders.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+// import { positions } from "../data/data";
+import axios from 'axios';
+
+
+
+
+
+
+const Orders = () => {
+  const [allOrders, setAllOrders] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3000/allOrders").then((res) => {
+      console.log(res.data);
+      setAllOrders(res.data);
+    })
+  }, []);
+  return (
+    <>
+    <h3 className="title">Total Orders ({allOrders.length})</h3>
+      <div className="order-table">
+        <div className="orders">
+        <table>
+          <tr>
+            <th>Order Name</th>
+            <th>Qty.</th>
+            <th>Price</th>
+            <th>Mode</th>
+          </tr>
+          {allOrders.map((stock, index)=>{
+              return (
+                <tr key={index}>
+                  <td>{stock.name}</td>
+                  <td>{stock.qty}.</td>
+                  <td>{stock.price}</td>
+                  <td>{stock.mode}</td>
+                </tr>
+              )
+            }
+          )}
+        </table>
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Orders;
